437c361f24a82016270d101eedb55e9b33766e40
vplechuc <plechuc@yahoo.com.ar>
2014-06-16 15:35:10 -0300

    refactor file name

--- src/fizz_buzz.rb
+++ src/fizz_buzz.rb
@@ -1,20 +1,22 @@
 def fizz_buzz(num)
   myArray=[]
 
-  1.upto(num).each do |i|
-
-    if (i % 3 == 0)
-      myArray[i] = 'Fizz'
-    elsif (i % 5 == 0)
-        myArray[i] = 'Buzz'
+  1.upto(num) do |num|
+    if (num % 3 == 0) and (num % 5 == 0)
+      myArray[num] = 'FizzBuzz'
     else
-      myArray[i] = i
+      if (num % 3 == 0)
+        myArray[num] = 'Fizz'
+      elsif (num % 5 == 0)
+        myArray[num] = 'Buzz'
+      else
+        myArray[num] = num
+      end
     end
-
   end
-  puts myArray
+   myArray
 end
 
-puts 'ingrese un valor'
-num = gets.chomp.to_i
-fizz_buzz(num)
+# puts 'ingrese un valor'
+# num = gets.chomp.to_i
+# fizz_buzz(num)
